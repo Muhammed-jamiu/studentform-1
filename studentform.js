e2214b2eab3f156b2183695c6166196849b52497
Anate Muhammed <anatemuhammed@gmail.com>
2024-08-26 14:26:40 +0100

    second commit

---
+++ studentform.js
@@ -0,0 +1,7 @@
+let clear = document.getElementById("clear");
+let Click = document.getElementById("Click");
+let message = document.getElementById("message");
+
+Click.addEventListener("click", () => {
+  message.innerHTML = "Your details is processing...";
+});
